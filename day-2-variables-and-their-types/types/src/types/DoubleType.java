dd5f480ea9e60edfcec0bd9702308e7ff69e83e3
gregory <gregorysak90@gmail.com>
2022-12-08 09:47:04 -0300

    feat: double type added

---
+++ day-2-variables-and-their-types/types/src/types/DoubleType.java
@@ -0,0 +1,15 @@
+package types;
+
+public class DoubleType {
+
+	public static void main(String[] args) {
+        double varDouble = 2.356;
+        double doubleRecebendoFloat = 1.409F;
+        double doubleRecebendoLong = 12930L;
+        System.out.println(varDouble);
+        System.out.println(doubleRecebendoFloat);
+        System.out.println(doubleRecebendoLong);
+
+	}
+
+}

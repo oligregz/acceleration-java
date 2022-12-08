946164749e388e79a64e653532e01e716a6b1c55
gregory <gregorysak90@gmail.com>
2022-12-08 10:32:54 -0300

    feat: boolean type added

---
+++ day-2-variables-and-their-types/types/src/types/BooleanType.java
@@ -0,0 +1,12 @@
+package types;
+
+public class BooleanType {
+
+	public static void main(String[] args) {
+        boolean isBoolean = true;
+        boolean isNumber = false;
+        System.out.println(isBoolean);
+        System.out.println(isNumber);
+	}
+
+}

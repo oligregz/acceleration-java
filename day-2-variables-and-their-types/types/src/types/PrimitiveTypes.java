18128f5285028fcb4a83c26d1d2f160fc0f0663f
gregory <gregorysak90@gmail.com>
2022-12-07 22:17:42 -0300

    feat: viewing types

--- day-2-variables-and-their-types/types/src/types/PrimitiveTypes.java
+++ day-2-variables-and-their-types/types/src/types/PrimitiveTypes.java
@@ -3,8 +3,25 @@ package types;
 public class PrimitiveTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        byte c = 9;
+        System.out.println(c);
+        
+        short num = 300;
+        short formattedWithdUnderscore = 20_000;
+        System.out.println(num);
+        System.out.println(formattedWithdUnderscore);
+        
+        int integer = 10;
+        int a = 10, b = 20, sum;
+        sum = a + b;
+        System.out.println(integer);
+        System.out.println(sum);
+        
+        long numA = 102040;
+        long numB = 102030405060L;
+        System.out.println(numA);
+        System.out.println(numB);
+        
 	}
 
 }
